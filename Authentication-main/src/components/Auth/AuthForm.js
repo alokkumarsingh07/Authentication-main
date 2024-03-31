2b27f636cff789d5f3401b7c57069be82f94ce7b
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-31 18:44:31 +0530

    Login

--- Authentication-main/src/components/Auth/AuthForm.js
+++ Authentication-main/src/components/Auth/AuthForm.js
@@ -1,12 +1,17 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
 
 import classes from "./AuthForm.module.css";
+import AuthContext from "../store/auth-context";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
-  const [isLogin, setIsLogin] = useState(true);
 
+  const authCtx = useContext(AuthContext);
+
+  const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
@@ -54,7 +59,8 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
+        history.replace("/");
       })
       .catch((err) => alert(err));
   };
